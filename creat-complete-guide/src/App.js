019795456c2de30b516525aecc007f942f103cb2
Sachin Mishra <D30001E226@asia.nidec.local>
2021-02-11 21:51:06 +0530

    First React App and JSX insites

---
+++ creat-complete-guide/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+
+class App extends Component {
+  render(){
+    return (
+
+      React.createElement('div',{className:'App'}, null,React.createElement('h1',null,'this is sachin and jsx'))
+
+    );
+  }
+}
+
+
+
+
+
+// const App = () => {
+//     return(
+// <div className='App'> 
+//   <h1>This is sachin mishra</h1>  
+// </div>
+
+//     );
+// }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hi i am a react app</h1>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
